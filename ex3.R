78dc4afd06b380afb4b6accdddad1886dc4c4954
Irell Zane <itsirellzane@gmail.com>
2024-06-05 11:50:18 +0100

    Fix ex3

--- ex3.R
+++ ex3.R
@@ -8,15 +8,21 @@ n <- 6
 
 mean_x <- sum_x/n
 mean_y <- sum_y/n
-c(mean_x, mean_y, mean_x2)
 
 b1_est <- (sum_xy - n * mean_x * mean_y) / (sum_x2 - n * mean_x^2)
 
 b0_est <- mean_y - b1_est * mean_x
 b0_est
 
-t <- (b0_est - h0_b0) / sqrt((1/n) + (mean_x^2 / (sum_x2 - n * mean_x^2)))
+var_est <- (1/(n-2))*((sum_y2-n*(mean_y^2)) - (b1_est^2)*(sum_x2-n*(mean_x^2)))
+t <- (b0_est - h0_b0) / sqrt(((1/n) + (mean_x^2 / (sum_x2 - n * mean_x^2) * var_est)))
 t
 
 alpha <- 0.01
+c(qt(alpha/2, n-2), qt(1 - alpha/2, n-2))
+
+alpha <- 0.05
+c(qt(alpha/2, n-2), qt(1 - alpha/2, n-2))
+
+alpha <- 0.10
 c(qt(alpha/2, n-2), qt(1 - alpha/2, n-2))
